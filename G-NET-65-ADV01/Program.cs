496c7a287eda73f91dd415371b2a13b683967cae
Asalah <Asalah@MSI>
2026-09-25 19:08:32 +0300

    Q14

--- G-NET-65-ADV01/Program.cs
+++ G-NET-65-ADV01/Program.cs
@@ -220,6 +220,30 @@ public class EntityManager<T> where T : Entity
              */
 
             #endregion
+            #region Q14
+            /*
+             
+             public class SafeList<T>
+{
+    private readonly List<T> _items = new List<T>();
+
+    public void Add(T item) => _items.Add(item);
+
+    public T GetAt(int index)
+    {
+        if (index < 0 || index >= _items.Count)
+        {
+            return default;
+        }
+        return _items[index];
+    }
+}
+             */
+
+            #endregion
+            #region MyRegion
+
+            #endregion
         }
     }
 }
