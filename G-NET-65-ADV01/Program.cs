d11b634e8e2c661865fde1345fa85c22586c712c
Asalah <Asalah@MSI>
2026-09-25 18:34:21 +0300

    Q1

--- G-NET-65-ADV01/Program.cs
+++ G-NET-65-ADV01/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            /*
+             Generic Class is a class that allows you to define a blueprint with type parameters
+            postponing the specification of the actual data types until the object is instantiated
+
+
+            WHY GENERICS ?
+
+            Type Safety: Catches type mismatch errors at compile-time
+
+            Performance: Eliminates the need for boxing and unboxing operations when working with value types
+
+            Code Reusability: Allows you to write a single implementation that works across multiple data types without duplicating code
+
+             
+             */
+            #endregion
         }
     }
 }
